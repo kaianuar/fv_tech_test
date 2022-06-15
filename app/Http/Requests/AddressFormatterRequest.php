ffbab85fade55546118816f65f7c536795643d63
kaianuar <anuar.jamlus@gmail.com>
2022-06-15 23:53:23 +0800

    Add custom request validation used by the API controller

---
+++ app/Http/Requests/AddressFormatterRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddressFormatterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'address1' => 'required|string',
+            'address2' => 'nullable',
+            'address3' => 'nullable',
+        ];
+    }
+}
