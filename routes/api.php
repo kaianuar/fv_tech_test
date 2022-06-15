a5f848a8b5551516a86a25957c56074a4ca4b086
kaianuar <anuar.jamlus@gmail.com>
2022-06-15 23:54:04 +0800

    API route to the AddressFormatterController

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/v1/address/verify', [\App\Http\Controllers\API\V1\AddressFormatterController::class, 'formatAddress'])->name('verifyAddress');
