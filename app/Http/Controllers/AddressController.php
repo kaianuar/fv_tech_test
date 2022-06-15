29b25f227e65804927f339cd8a71639b9c088f0d
kaianuar <anuar.jamlus@gmail.com>
2022-06-15 23:55:04 +0800

    Add web controller to return the address form view

---
+++ app/Http/Controllers/AddressController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AddressController extends Controller
+{
+    public function index()
+    {
+        return view('address/index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/address-form', [\App\Http\Controllers\AddressController::class, 'index']);
